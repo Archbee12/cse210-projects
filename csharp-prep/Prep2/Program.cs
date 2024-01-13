a2ced397f883a6c3239f8bace87c6bab23ea7ec8
Archbee12 <debbieokon32@gmail.com>
2024-01-13 03:42:37 -0800

    Finished with Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,74 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percent? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        string letter = "";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+
+        string sign = " ";
+        int last_digit = gradeNumber % 10;
+
+        if (last_digit >= 7)
+        {
+            sign = "+";
+        }
+        else if (last_digit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+
+        if (gradeNumber >= 93)
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations, You made it.");
+        }
+        else
+        {
+            Console.WriteLine("Try better next time.");
+        }
+
     }
 }
+
+
